18d823e0a3a8010b1980dd45765eff0d027b1728
Andrei Lavrov <lavrovvandrei@gmail.com>
2023-03-15 14:01:44 +0300

    feat/PSYC-17 Create minimally working "TODO" telegram chat with PostgreSQL DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,38 @@
-import { TelegrafModule } from 'nestjs-telegraf';
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { TelegrafModule } from 'nestjs-telegraf';
+import { join } from 'path'
+import * as LocalSession from 'telegraf-session-local';
+import { AppService } from './app.service';
 import { AppUpdate } from 'src/app.update';
 import { ConfigService } from 'src/config/config.service';
-import { sessionMiddleware } from 'src/middleware/session.middleware';
+import { TaskEntity } from './task.entity'
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+const sessions = new LocalSession({  database: 'session_db.json'});
 const conf = new ConfigService;
 
 @Module({
   imports: [
-    AppUpdate,
     TelegrafModule.forRoot({
-      botName: 'psychology_the_best_bot',
+      middlewares: [sessions.middleware()],
       token: conf.get('TOKEN'),
-      middlewares: [sessionMiddleware],
-    })
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'todo-app-tg-bot',
+      username: 'postgres',
+      password: '007',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      migrations: [join(__dirname, '**', '*.migration.{ts,js}')],
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+    TypeOrmModule.forFeature([TaskEntity])
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppUpdate],
 })
 export class AppModule {}
